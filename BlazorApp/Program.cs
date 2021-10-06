68b3cec6dd8058a17a3022b30a807bfe3723d0db
Mike Jensen <43805784+Shimitan@users.noreply.github.com>
2021-10-06 13:08:33 +0200

    Blazor Template inserted

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace P3_WebApp
+namespace BlazorApp
 {
     public class Program
     {
@@ -18,6 +18,9 @@ namespace P3_WebApp
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
