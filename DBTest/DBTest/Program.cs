0c2849973b268f2561260b691a64fdb1af904bb5
Andreas <andreas090994@gmail.com>
2021-10-06 12:39:53 +0200

    local fix

--- DBTest/DBTest/Program.cs
+++ DBTest/DBTest/Program.cs
@@ -7,27 +7,24 @@ namespace DBTest
     {
         static void Main(string[] args)
         {
-            string connectionString = "Server=DESKTOP-91DJ17V\\SQLEXPRESS01;Database=ANS_CUSTOM_CI; User ID=aes; Password=aes;Trusted_Connection=False";
+            string connectionString = "Server=localhost;Database=ANS_CUSTOM_CI; User ID=aes; Password=Password123;Trusted_Connection=False";
+            string queryString = "SELECT * FROM dbo.AFSTEMNING";
             try
             {
-                string queryString = "SELECT * FROM dbo.AFSTEMNING";
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
 
                     Console.WriteLine("State: {0}", connection.State);
-                    Console.WriteLine("ConnectionString: {0}",
-                        connection.ConnectionString);
+                    Console.WriteLine("ConnectionString: {0}", connection.ConnectionString);
                     
-                    SqlCommand command = new SqlCommand(
-                        queryString, connection);
+                    SqlCommand command = new SqlCommand(queryString, connection);
 
                     using(SqlDataReader reader = command.ExecuteReader())
                     {
                         while (reader.Read())
                         {
-                            Console.WriteLine("{0}, {1}",
-                                reader[0], reader[1]);
+                            Console.WriteLine("{0}, {1}, {2}", reader[1], reader[3], reader[6]);
                         }
                     }
                 }
@@ -36,7 +33,6 @@ namespace DBTest
             {
                 Console.WriteLine(e);
             }
-            
         }
     }
 }
