14898f85c33e862e2cdc2b2215baf7c96e5ae4ed
Andreas ahlbeck <aahlbe20@student.aau.dk>
2021-11-16 13:28:40 +0100

    Added data for memory axis

--- BlazorApp/Data/Test.cs
+++ BlazorApp/Data/Test.cs
@@ -22,18 +22,30 @@ namespace BlazorApp.Data
         
         private List<HealthDataTest> usage = new List<HealthDataTest>()
         {
-            new HealthDataTest("CPU data", 70, Convert.ToDateTime("2021-09-09 12:12")),
-            new HealthDataTest("CPU data", 90, Convert.ToDateTime("2021-09-09 12:13")),
-            new HealthDataTest("CPU data", 87, Convert.ToDateTime("2021-09-09 12:14")),
-            new HealthDataTest("CPU data", 50, Convert.ToDateTime("2021-09-09 12:15")),
-            new HealthDataTest("CPU data", 87, Convert.ToDateTime("2021-09-09 12:16")),
-            new HealthDataTest("CPU data", 50, Convert.ToDateTime("2021-09-09 12:17")),
-            new HealthDataTest("CPU data", 70, Convert.ToDateTime("2021-09-09 12:18")),
-            new HealthDataTest("CPU data", 40, Convert.ToDateTime("2021-09-09 12:19")),
-            new HealthDataTest("CPU data", 70, Convert.ToDateTime("2021-09-09 12:20")),
-            new HealthDataTest("CPU data", 43, Convert.ToDateTime("2021-09-09 12:21")),
-            new HealthDataTest("CPU data", 20, Convert.ToDateTime("2021-09-09 12:22")),
-            new HealthDataTest("CPU data", 100,Convert.ToDateTime("2021-09-09 12:23"))
+            new HealthDataTest("CPU", 70, Convert.ToDateTime("2021-09-09 12:12")),
+            new HealthDataTest("CPU", 90, Convert.ToDateTime("2021-09-09 12:13")),
+            new HealthDataTest("CPU", 87, Convert.ToDateTime("2021-09-09 12:14")),
+            new HealthDataTest("CPU", 50, Convert.ToDateTime("2021-09-09 12:15")),
+            new HealthDataTest("CPU", 87, Convert.ToDateTime("2021-09-09 12:16")),
+            new HealthDataTest("CPU", 50, Convert.ToDateTime("2021-09-09 12:17")),
+            new HealthDataTest("CPU", 70, Convert.ToDateTime("2021-09-09 12:18")),
+            new HealthDataTest("CPU", 40, Convert.ToDateTime("2021-09-09 12:19")),
+            new HealthDataTest("CPU", 70, Convert.ToDateTime("2021-09-09 12:20")),
+            new HealthDataTest("CPU", 43, Convert.ToDateTime("2021-09-09 12:21")),
+            new HealthDataTest("CPU", 20, Convert.ToDateTime("2021-09-09 12:22")),
+            new HealthDataTest("CPU", 100,Convert.ToDateTime("2021-09-09 12:23")),
+            new HealthDataTest("MEMORY", 12, Convert.ToDateTime("2021-09-09 12:12")),
+            new HealthDataTest("MEMORY", 41, Convert.ToDateTime("2021-09-09 12:13")),
+            new HealthDataTest("MEMORY", 11, Convert.ToDateTime("2021-09-09 12:14")),
+            new HealthDataTest("MEMORY", 59, Convert.ToDateTime("2021-09-09 12:15")),
+            new HealthDataTest("MEMORY", 8, Convert.ToDateTime("2021-09-09 12:16")),
+            new HealthDataTest("MEMORY", 5, Convert.ToDateTime("2021-09-09 12:17")),
+            new HealthDataTest("MEMORY", 7, Convert.ToDateTime("2021-09-09 12:18")),
+            new HealthDataTest("MEMORY", 10, Convert.ToDateTime("2021-09-09 12:19")),
+            new HealthDataTest("MEMORY", 80, Convert.ToDateTime("2021-09-09 12:20")),
+            new HealthDataTest("MEMORY", 3, Convert.ToDateTime("2021-09-09 12:21")),
+            new HealthDataTest("MEMORY", 2, Convert.ToDateTime("2021-09-09 12:22")),
+            new HealthDataTest("MEMORY", 10,Convert.ToDateTime("2021-09-09 12:23"))
         };
         public async Task<List<HealthDataTest>> DataList()
         {
