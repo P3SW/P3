d2a190737be6fa062abf376a5b2fcad21b8d876f
Mike Jensen <mike.jensen.at@gmail.com>
2021-11-17 23:32:41 +0100

    Fixing overflow on tables WIP

--- BlazorApp/TestData/AnotherTest.cs
+++ BlazorApp/TestData/AnotherTest.cs
@@ -8,11 +8,46 @@ namespace BlazorApp.Data
     {
         List<Error> err = new List<Error>()
         {
+            new Error(1, "INFO", Convert.ToDateTime("01-May-2021"), "This is a meesage"),
+            new Error(2, "INFO", Convert.ToDateTime("05-May-2021"), "This is a message"),
+            new Error(3, "WARNING", Convert.ToDateTime("01-May-2021"), "This is not a message"),
+            new Error(4, "FATAL", Convert.ToDateTime("10-May-2021"), "This is a message"),
+            new Error(5, "ERROR", Convert.ToDateTime("21-May-2021"), "This is a message"),
             new Error(1, "INFO", Convert.ToDateTime("01-May-2021"), "This is a message"),
             new Error(2, "INFO", Convert.ToDateTime("05-May-2021"), "This is a message"),
             new Error(3, "WARNING", Convert.ToDateTime("01-May-2021"), "This is not a message"),
             new Error(4, "FATAL", Convert.ToDateTime("10-May-2021"), "This is a message"),
-            new Error(5, "ERROR", Convert.ToDateTime("21-May-2021"), "This is a message")
+            new Error(5, "ERROR", Convert.ToDateTime("21-May-2021"), "This is a message"),
+            new Error(1, "INFO", Convert.ToDateTime("01-May-2021"), "This is a message"),
+            new Error(2, "INFO", Convert.ToDateTime("05-May-2021"), "This is a message"),
+            new Error(3, "WARNING", Convert.ToDateTime("01-May-2021"), "This is not a message"),
+            new Error(4, "FATAL", Convert.ToDateTime("10-May-2021"), "This is a message"),
+            new Error(5, "ERROR", Convert.ToDateTime("21-May-2021"), "This is a message"),
+            new Error(1, "INFO", Convert.ToDateTime("01-May-2021"), "This is a message"),
+            new Error(2, "INFO", Convert.ToDateTime("05-May-2021"), "This is a message"),
+            new Error(3, "WARNING", Convert.ToDateTime("01-May-2021"), "This is not a message"),
+            new Error(4, "FATAL", Convert.ToDateTime("10-May-2021"), "This is a message"),
+            new Error(5, "ERROR", Convert.ToDateTime("21-May-2021"), "This is a message"),
+            new Error(1, "INFO", Convert.ToDateTime("01-May-2021"), "This is a message"),
+            new Error(2, "INFO", Convert.ToDateTime("05-May-2021"), "This is a message"),
+            new Error(3, "WARNING", Convert.ToDateTime("01-May-2021"), "This is not a message"),
+            new Error(4, "FATAL", Convert.ToDateTime("10-May-2021"), "This is a message"),
+            new Error(5, "ERROR", Convert.ToDateTime("21-May-2021"), "This is a message"),
+            new Error(1, "INFO", Convert.ToDateTime("01-May-2021"), "This is a message"),
+            new Error(2, "INFO", Convert.ToDateTime("05-May-2021"), "This is a message"),
+            new Error(3, "WARNING", Convert.ToDateTime("01-May-2021"), "This is not a message"),
+            new Error(4, "FATAL", Convert.ToDateTime("10-May-2021"), "This is a message"),
+            new Error(5, "ERROR", Convert.ToDateTime("21-May-2021"), "This is a message"),
+            new Error(1, "INFO", Convert.ToDateTime("01-May-2021"), "This is a message"),
+            new Error(2, "INFO", Convert.ToDateTime("05-May-2021"), "This is a message"),
+            new Error(3, "WARNING", Convert.ToDateTime("01-May-2021"), "This is not a message"),
+            new Error(4, "FATAL", Convert.ToDateTime("10-May-2021"), "This is a message"),
+            new Error(5, "ERROR", Convert.ToDateTime("21-May-2021"), "This is a message"),
+            new Error(1, "INFO", Convert.ToDateTime("01-May-2021"), "This is a message"),
+            new Error(2, "INFO", Convert.ToDateTime("05-May-2021"), "This is a message"),
+            new Error(3, "WARNING", Convert.ToDateTime("01-May-2021"), "This is not a message"),
+            new Error(4, "FATAL", Convert.ToDateTime("10-May-2021"), "This is a message"),
+            new Error(5000, "ERROR", Convert.ToDateTime("21-May-2021"), "This is a message")
         };
 
         public async Task<List<Error>> ErrorList()
@@ -21,7 +56,8 @@ namespace BlazorApp.Data
         }
         public List<Error> GetErrors()
         {
-            err.Add(new Error(1, "INFO", Convert.ToDateTime("01-May-2021"), "This is a message"));
+            err.Add(new Error(5001, "INFO", Convert.ToDateTime("01-May-2021"), "This is a message"));
+            err.Add(new Error(5002, "INFO", Convert.ToDateTime("01-May-2021"), "This is a message"));
             return err;
         }
     }
