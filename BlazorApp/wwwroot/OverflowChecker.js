758d7f3453666f1d8f05681d09d2cb6ec13beec3
Mike Jensen <43805784+Shimitan@users.noreply.github.com>
2021-11-23 08:16:42 +0100

    WIP

---
+++ BlazorApp/wwwroot/OverflowChecker.js
@@ -0,0 +1,10 @@
+﻿function isOverflown(element) {
+    return element.scrollHeight > element.clientHeight || element.scrollWidth > element.clientWidth;
+}
+
+var els = document.getElementsByClassName('demos');
+for (var i = 0; i < els.length; i++) {
+    var el = els[i];
+    el.style.borderColor = (isOverflown(el) ? 'red' : 'green');
+    console.log("Element #" + i + " is " + (isOverflown(el) ? '' : 'not ') + "overflown.");
+}
