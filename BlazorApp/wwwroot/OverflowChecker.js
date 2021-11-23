dd3ade8542c2f7c329fd33b0aa222d74c04b2701
Mike Jensen <mike.jensen.at@gmail.com>
2021-11-23 10:28:48 +0100

    WIP overflow checker
    Currently works in .razor files, but not in Radzen.

--- BlazorApp/wwwroot/OverflowChecker.js
+++ BlazorApp/wwwroot/OverflowChecker.js
@@ -1,10 +1,12 @@
-﻿function isOverflown(element) {
+﻿/* Check if an element is overflowing its parent */
+export function isOverflown(element) {
     return element.scrollHeight > element.clientHeight || element.scrollWidth > element.clientWidth;
 }
 
-var els = document.getElementsByClassName('demos');
-for (var i = 0; i < els.length; i++) {
-    var el = els[i];
-    el.style.borderColor = (isOverflown(el) ? 'red' : 'green');
-    console.log("Element #" + i + " is " + (isOverflown(el) ? '' : 'not ') + "overflown.");
+/* Automatically checks all elements with a given classname for overflow */
+var elements = document.getElementsByClassName('overflowCheck');
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+    element.style.color = (isOverflown(element) ? 'red' : 'green');
+    console.log("Element #" + i + " is " + (isOverflown(element) ? '' : 'not ') + "overflown.");
 }
