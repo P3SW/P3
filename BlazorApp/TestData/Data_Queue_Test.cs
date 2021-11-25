5d3dff72c6061d1fa17ae0687efe072569a2bfe6
Alexander Steffensen <74182126+AlexanderSteffensen@users.noreply.github.com>
2021-11-25 09:46:12 +0100

    deleted comma

--- BlazorApp/TestData/Data_Queue_Test.cs
+++ BlazorApp/TestData/Data_Queue_Test.cs
@@ -195,7 +195,7 @@ namespace BlazorApp.Data
                 new Reconciliation(2, "OK", Convert.ToDateTime("05-May-2021"), "This is a message"),
                 new Reconciliation(3, "DISABLED", Convert.ToDateTime("01-May-2021"), "This is a message"),
                 new Reconciliation(4, "OK", Convert.ToDateTime("10-May-2021"), "This is a message"),
-            }),
+            })
         };
         
         public async Task<List<Manager_test>> ManagerList()
