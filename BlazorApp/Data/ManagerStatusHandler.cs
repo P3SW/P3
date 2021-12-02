7f200b999aac12ccd712356247d038400958f4d0
Mike Jensen <43805784+Shimitan@users.noreply.github.com>
2021-12-02 13:35:24 +0100

    Temp fix for the overflow on efficiency score

--- BlazorApp/Data/ManagerStatusHandler.cs
+++ BlazorApp/Data/ManagerStatusHandler.cs
@@ -92,7 +92,17 @@ namespace BlazorApp.Data
         {
             AvgCpu = Health.Cpu.Count > 0 ?  Health.Cpu.Average(data => data.NumericValue) : 0.0;
             Cpu = Convert.ToInt32(AvgCpu);
-            double result = ((double) (RowsRead + RowsWritten) / RunTime * (1+AvgCpu))*10;
+
+            double result;
+            if (RunTime == 0)
+            {
+                result = 0;
+            }
+            else
+            {
+                result = ((double) (RowsRead + RowsWritten) / RunTime * (1+AvgCpu))*10;
+            }
+            
             EfficiencyScore = Convert.ToInt32(result);
         }
         
