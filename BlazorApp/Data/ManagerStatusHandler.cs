99bd50a2284d7aa1b6793c0b2e443bd42ca41029
Andreas <andreas090994@gmail.com>
2021-12-07 15:18:59 +0100

    Display line chart specific data for each manager + 'Track current manager' feature (#94)
    * LineChart is manager specific - BUG: manager 0, Onclick crash
    
    * LineChart manager split + follow current manager checkbox
    
    * Follow current manager checkbox css added (TODO: fix)
    
    * Fixed data bind + OnCheckboxClick()
    
    * Track current manager remberes linechart dropdown selection + Moved line chart to method
    
    * Clean up
    
    * Null check
    
    * Moved CpuDataList + MemDataList from ConversionDataAssigner to ManagerStatusHandler
    
    * Rename CurrentManager
    
    * Fixed errors

--- BlazorApp/Data/ManagerStatusHandler.cs
+++ BlazorApp/Data/ManagerStatusHandler.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using BlazorApp.DataStreaming.Events;
@@ -38,6 +39,8 @@ namespace BlazorApp.Data
         public long MemoryUsed;
         public long MaxMemory = 21473734656; /* Approx 20gb */
 
+        public readonly List<HealthData> CpuDataList = new ();
+        public readonly List<HealthData> MemDataList = new ();
 
         public ManagerStatusHandler(string name, int id, DateTime startTime, int executionId)
         {
