508cedf79a09f5b4745dcc3ee1ec946b23eeca35
Mike Jensen <43805784+Shimitan@users.noreply.github.com>
2021-12-07 15:18:20 +0100

    Tidy + renaming

--- BlazorApp/Data/ManagerStatusHandler.cs
+++ BlazorApp/Data/ManagerStatusHandler.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using System.Threading;
+using BlazorApp.Data.Events;
 using BlazorApp.DataStreaming.Events;
 using Microsoft.Data.SqlClient;
 using SQLDatabaseRead;
